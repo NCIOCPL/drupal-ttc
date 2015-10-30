e61ddb55459a3298f0eb6940fd02adbebcbc02e1
burelbachwg <webcommauto@users.noreply.github.com>
2015-10-30 20:51:51 +0000

    [OCPLTTC-45] Added blocks and most of the small breakpoint ("smartphone") styling to the footer.

--- drupal/sites/default/themes/ttctheme/template.php
+++ drupal/sites/default/themes/ttctheme/template.php
@@ -4,6 +4,7 @@
  * Implements template_preprocess_html().
  */
 function ttctheme_preprocess_html(&$variables) {
+  drupal_add_css('https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic', array('group' => CSS_THEME));
 }
 
 /**
