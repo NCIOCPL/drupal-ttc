673a62d6e4d8d3e4eed9b3e0c149994b35f190db
Dan Young <youngdr@users.noreply.github.com>
2015-11-23 19:36:05 +0000

    Added facet theme function override for inactive links to point the generated urls back to the /availabletechnologies search page.

--- drupal/sites/default/themes/ttctheme/template.php
+++ drupal/sites/default/themes/ttctheme/template.php
@@ -12,18 +12,19 @@ function ttctheme_preprocess_html(&$variables) {
  */
 function ttctheme_preprocess_page(&$variables) {
   $variables['show_alt_header'] = (
-    $variables['linked_logo'] ||
-    $variables['site_name'] ||
-    $variables['site_slogan'] ||
-    $variables['alt_main_menu'] ||
-    $variables['alt_secondary_menu']
-  );
+      $variables['linked_logo'] ||
+      $variables['site_name'] ||
+      $variables['site_slogan'] ||
+      $variables['alt_main_menu'] ||
+      $variables['alt_secondary_menu']
+      );
 }
 
 /**
  * Implements template_preprocess_node().
  */
 function ttctheme_preprocess_node(&$variables) {
+  
 }
 
 /**
@@ -63,8 +64,7 @@ function ttctheme_form_alter(&$form, &$form_state, $form_id) {
 
     $form['actions']['submit']['#value'] = 'Go';
     $form['actions']['submit']['#attributes']['class'] = array_diff(
-      $form['actions']['submit']['#attributes']['class'],
-      array('secondary')
+        $form['actions']['submit']['#attributes']['class'], array('secondary')
     );
     $form['actions']['submit']['#attributes']['class'][] = 'primary';
     $form['actions']['submit']['#attributes']['class'][] = 'search-form__submit';
@@ -72,7 +72,38 @@ function ttctheme_form_alter(&$form, &$form_state, $form_id) {
   }
 }
 
-
 function regex_to_lowercase($matches) {
   return strtolower($matches[0]);
+}
+
+function ttctheme_facetapi_link_inactive($variables) {
+
+  $node = menu_get_object();
+
+  if ($node && $node->type == 'abstract') {
+    $variables['path'] = 'availabletechnologies/all';
+  }
+
+  // Builds accessible markup.
+  // @see http://drupal.org/node/1316580
+  $accessible_vars = array(
+    'text' => $variables['text'],
+    'active' => FALSE,
+  );
+  $accessible_markup = theme('facetapi_accessible_markup', $accessible_vars);
+
+  // Sanitizes the link text if necessary.
+  $sanitize = empty($variables['options']['html']);
+  $variables['text'] = ($sanitize) ? check_plain($variables['text']) : $variables['text'];
+
+  // Adds count to link if one was passed.
+  if (isset($variables['count'])) {
+    $variables['text'] .= ' ' . theme('facetapi_count', $variables);
+  }
+
+  // Resets link text, sets to options to HTML since we already sanitized the
+  // link text and are providing additional markup for accessibility.
+  $variables['text'] .= $accessible_markup;
+  $variables['options']['html'] = TRUE;
+  return theme_link($variables);
 }
