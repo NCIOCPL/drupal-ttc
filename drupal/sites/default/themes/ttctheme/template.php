8cde7acfb6d1b31b5047ab5ea5bb1ffc5e10bffa
Dan Young <youngdr@users.noreply.github.com>
2015-11-24 19:56:08 +0000

    Updated the active facet styling to look like the inactive facet styling.

--- drupal/sites/default/themes/ttctheme/template.php
+++ drupal/sites/default/themes/ttctheme/template.php
@@ -157,3 +157,28 @@ function ttctheme_facetapi_link_inactive($variables) {
   $variables['options']['html'] = TRUE;
   return theme_link($variables);
 }
+
+function ttctheme_facetapi_link_active($variables) {
+  // Builds accessible markup.
+  // @see http://drupal.org/node/1316580
+  $accessible_vars = array(
+    'text' => $variables['text'],
+    'active' => FALSE,
+  );
+  $accessible_markup = theme('facetapi_accessible_markup', $accessible_vars);
+  
+  // Sanitizes the link text if necessary.
+  $sanitize = empty($variables['options']['html']);
+  $variables['text'] = ($sanitize) ? check_plain($variables['text']) : $variables['text'];
+  
+  // Adds count to link if one was passed.
+  if (isset($variables['count'])) {
+    $variables['text'] .= ' ' . theme('facetapi_count', $variables);
+  }
+
+  // Resets link text, sets to options to HTML since we already sanitized the
+  // link text and are providing additional markup for accessibility.
+  $variables['text'] .= $accessible_markup;
+  $variables['options']['html'] = TRUE;
+  return theme_link($variables);
+}
