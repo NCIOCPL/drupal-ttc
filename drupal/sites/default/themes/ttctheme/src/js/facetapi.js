744d01a033e0605b19129f3ca8b943947375ac78
burelbachwg <webcommauto@users.noreply.github.com>
2015-12-08 19:01:55 +0000

    [OCPLTTC-64] Added some styling for the Available Technologies search results page. Changed the links in the faceted search block to checkboxes.

---
+++ drupal/sites/default/themes/ttctheme/src/js/facetapi.js
@@ -0,0 +1,65 @@
+(function () {
+  'use strict';
+
+  Drupal.facetapi.makeCheckbox = function () {
+    var $link = jQuery(this);
+    var isActive = $link.hasClass('facetapi-active');
+    var isInactive = $link.hasClass('facetapi-inactive');
+
+    if (!isActive && !isInactive) {
+      // Not a facet link.
+      return;
+    }
+
+    // Derive an ID and label for the checkbox based on the associated link.
+    // The label is required for accessibility, but it duplicates information
+    // in the link itself, so it should only be shown to screen reader users.
+    var id = this.id + '--checkbox';
+    var isDisabled = $link.hasClass('facetapi-zero-results');
+
+    // Get just the link label, without text inside an invisible element.
+    var description = $link.contents().filter(function () {
+      return this.nodeType === 3;
+    })[0].nodeValue;
+
+    // Create the elements for insertion.
+    var $container = jQuery('<div class="checkbox">');
+    var $label = jQuery('<label for="' + id + '">' + description + '</label>');
+    var $checkbox = jQuery('<input type="checkbox" class="facetapi-checkbox" id="' + id + '">');
+
+    var href = $link.attr('href');
+    var redirect = new Drupal.facetapi.Redirect(href);
+    var filterAction;
+
+    $checkbox.click(function (e) {
+      Drupal.facetapi.disableFacet($link.parents('ul.facetapi-facetapi-checkbox-links'));
+      redirect.gotoHref();
+    });
+
+    if (isActive) {
+      $checkbox.prop('checked', true);
+      filterAction = Drupal.t('Remove filter for: ');
+    }
+    else {
+      filterAction = Drupal.t('Apply filter for: ');
+    }
+
+    if (isDisabled) {
+      $checkbox.prop('disabled', true);
+    }
+
+    // Add accessible text to the label
+    $label.prepend('<span class="element-invisible">' + filterAction + '</span>');
+
+    // Set up the container
+    // Example markup:
+    // <div class="checkbox">
+    //   <input id="ID" type="checkbox">
+    //   <label for="ID">LABEL</label>
+    // </div>
+    $container.append($checkbox).append($label);
+    // Add the checkbox and label, remove the link
+    $link.after($container)
+      .remove();
+  };
+}());
