fda93c82de0e44bc62280fac36ba654b1112f73c
ahmadz2 <webcommauto@users.noreply.github.com>
2016-01-13 19:33:23 +0000

    success stories - fixed view structure - removed need for javascript wrapper, brought into view.

--- drupal/sites/default/themes/ttctheme/src/js/ttctheme.js
+++ drupal/sites/default/themes/ttctheme/src/js/ttctheme.js
@@ -2,7 +2,6 @@
   'use strict';
   $(document).ready(function () {
     // Create a link to remove the keyword filter.
-    $('.success-stories__list li').wrapInner("<div class='listwrapper' />");
     // match column heights
     $('.listwrapper img').each(function () {
       var highestBox = $('.listwrapper img').height();
