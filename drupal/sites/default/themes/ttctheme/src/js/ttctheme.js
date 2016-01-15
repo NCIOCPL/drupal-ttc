2eb7aad8b6f31b4c0528fb6fb8789ea69d1263f5
ahmadz2 <webcommauto@users.noreply.github.com>
2016-01-15 16:01:57 +0000

    Updated theme and javascript files to prep for two new blocks on hompage.

--- drupal/sites/default/themes/ttctheme/src/js/ttctheme.js
+++ drupal/sites/default/themes/ttctheme/src/js/ttctheme.js
@@ -7,6 +7,7 @@
       var highestBox = $('.listwrapper img').height();
       $(this).height(highestBox);
     });
+    $('.pane-menu-menu-information ul li.expanded').wrapInner('<div class="li-wrapper"></div>');
     $('body.page-availabletechnologies #search-block-form').clone().addClass('at-search').insertBefore('h2.pane-title');
     $('<h2 class="text-left">Search</h2>').insertBefore('.at-search');
     $('<h2 class="text-left">Browse</h2>').insertAfter('body.page-availabletechnologies .pane-available-technologies .pane-title');
