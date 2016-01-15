aeab833721f5d67063ba258dabd57df560990b85
ahmadz2 <webcommauto@users.noreply.github.com>
2016-01-15 17:56:12 +0000

    https://tracker.nci.nih.gov/browse/OCPLTTC-30 Homepage CSS Cleanup - adjusted padding and border on block items - top and bottom padding on panels - minor padding top fix to available tech page - removed link attr from top level menu links in information page. - basic styling fixes to match comps

--- drupal/sites/default/themes/ttctheme/src/js/ttctheme.js
+++ drupal/sites/default/themes/ttctheme/src/js/ttctheme.js
@@ -11,5 +11,9 @@
     $('body.page-availabletechnologies #search-block-form').clone().addClass('at-search').insertBefore('h2.pane-title');
     $('<h2 class="text-left">Search</h2>').insertBefore('.at-search');
     $('<h2 class="text-left">Browse</h2>').insertAfter('body.page-availabletechnologies .pane-available-technologies .pane-title');
+    //  prevent first level menu in information panel from being link
+    $('.pane-menu-menu-information a.active').on('click', function (e) {
+      e.preventDefault();
+    });
   });
 }(jQuery));
