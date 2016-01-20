4c62b856c4d3b217adf1a8e6077a8c16cdd5ffaf
ahmadz2 <webcommauto@users.noreply.github.com>
2016-01-20 04:19:26 +0000

    fixed success stories images - added js to create the url of the image into the background of the parent div - adjusted some minor css

--- drupal/sites/default/themes/ttctheme/src/js/ttctheme.js
+++ drupal/sites/default/themes/ttctheme/src/js/ttctheme.js
@@ -11,11 +11,20 @@
     $('body.page-availabletechnologies #search-block-form').clone().addClass('at-search').insertBefore('h2.pane-title');
     $('<h2 class="text-left">Search</h2>').insertBefore('.at-search');
     $('<h2 class="text-left">Browse</h2>').insertAfter('body.page-availabletechnologies .pane-available-technologies .pane-title');
+
     //  prevent first level menu in information panel from being link
     $('.pane-menu-menu-information a.active').on('click', function (e) {
       e.preventDefault();
     });
+
     // social links target blank new window
     $('.follow-us__list-item a').attr('target', '_blank');
+
+    // success stories images into background image of parent
+    $('.success-stories__image img').each(function () {
+      var thisurl = $(this).attr('src');
+      $(this).parent('.success-stories__image').css('background-image', 'url(' + thisurl + ')');
+    });
+
   });
 }(jQuery));
