4543afe5b66618bec82990d989d0afe1429c91ea
Dan Young <youngdr@users.noreply.github.com>
2018-03-30 16:02:37 -0400

    Truncate xml data and revision tables before rebuilding data

--- save-ott-nodes.php
+++ save-ott-nodes.php
@@ -1,5 +1,12 @@
 <?php
 
+// clear all xml data
+$result = db_truncate('field_data_field_xml')->execute();
+drush_log("XML data truncate result: $result", 'ok');
+
+$result = db_truncate('field_revision_field_xml')->execute();
+drush_log("XML revision truncate result: $result", 'ok');
+
 $result = db_query('SELECT entity_id as nid FROM field_data_field_send_to_ott where field_send_to_ott_value = 1');
 $nids = $result->fetchCol(0);
 $nodes = node_load_multiple($nids);
