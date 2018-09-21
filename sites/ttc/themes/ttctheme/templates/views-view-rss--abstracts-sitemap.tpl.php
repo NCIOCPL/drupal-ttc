f484745fee7e07c0920bee3d28f86c2d503f6179
Kalidindi, Ramaraju (NIH/NCI) [C] <ramaraju.kalidindi@nih.gov>
2018-09-21 14:50:38 -0400

    Changes for meta keywords.

--- sites/ttc/themes/ttctheme/templates/views-view-rss--abstracts-sitemap.tpl.php
+++ sites/ttc/themes/ttctheme/templates/views-view-rss--abstracts-sitemap.tpl.php
@@ -10,5 +10,5 @@
 <?php print "<?xml"; ?> version="1.0" encoding="UTF-8" <?php print "?>"; ?>
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-<?php print $items; ?>
+  <?php print $items; ?>
 </urlset>
